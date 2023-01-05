63315a15bd945e5da2cbc8d7b29100daa751981a
Eugene Masaka <eugenemasaka777@gmail.com>
2023-01-05 10:31:23 +0300

    Connecting to an ethereum node

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("dotenv").config({ path: ".env"});
 
+const INFURA_HTTP_URL = process.env.INFURA_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+    solidity: "0.8.9",
+    networks: {
+        goerli: {
+            url: INFURA_HTTP_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+};
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
